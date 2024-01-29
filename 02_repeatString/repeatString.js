e4ac5dae0725b5be87b6326653c3443b0b6380a2
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-01-30 01:02:42 +0530

    add solutions to first 4 problems

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let i = num;
+  let str = '';
+  if (num>=0)
+  {
+    while(i>0){
+      str += string;
+      i--;
+    }
+    return str;
+  }
+  else 
+  { 
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...theArgs) {
+  let updated = [];
+  arr.forEach((item) => {
+    if (!theArgs.includes(item)) {
+      updated.push(item);
+    }
+  });
+  return updated;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
