072315551af4f015914a59dcfae7ed1a6dca02b1
Adamnson-0dinson <theawakeavatar@gmail.com>
2024-01-31 12:15:45 +0530

    add solutions for q5-q8

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  let sum = 0;
+  let typeCondition = (typeof(start) === "number" && typeof(end) === "number");
+  let negativeCondition = (start >= 0 && end >= 0);
+  if(typeCondition && negativeCondition){
+    let start_i = 0;
+    let end_i = 0;
 
+    if( start <= end)
+    {
+      start_i = start;
+      end_i = end;
+    }
+    else {
+      start_i = end;
+      end_i = start;
+    }
+
+    for(let i = start_i ; i <= end_i; i++){
+      sum += i;
+    }
+
+    return sum;
+  }
+  else { return "ERROR";}
+  
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return (a+b);	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total =0;
+  a.forEach(element => {
+    total += element;
+  });	
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let product = 1;
+  a.forEach(el => {
+    product *= el;
+  });
+  return product;
 
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	let ans = 1;
+  for(let i=1; i<=power; i++)
+  {
+    ans *= number;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if(number === 0 || number === 1) {
+    return 1;
+  }
+  else {
+    for(let i=number; i>=1; i--){
+      return number*factorial(number-1);
+    }
+  }
 };
 
 // Do not edit below this line
